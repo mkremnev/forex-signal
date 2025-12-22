046e2faab88ab045b8419cf239a3ad1b7a9b9925
Maxim Kremnev <kalaus_1@mail.ru>
2025-12-22 19:05:42 +0300

    Update

--- packages/dashboard/config/config.php
+++ packages/dashboard/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'app.name' => 'Boutique Boss',
-    'session.name' => 'boutique_boss',
-    'sec-key' => '28fa7bc7-65d0-444e-87ba-7c4b55b4d768',
-    'i18n' => 'ru',
+    "app.name" => "Forex signall dashboard",
+    "session.name" => "forex_signall",
+    "sec-key" => getenv("SEC_KEY"),
+    "i18n" => "ru",
 ];
